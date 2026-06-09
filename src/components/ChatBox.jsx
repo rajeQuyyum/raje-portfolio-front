33931d8c67ff7cb5d44d1f824eef99f5a93e9b0e
Raje <rajevictory@gmail.com>
2026-06-09 19:22:52 +0100

    new

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import io from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
-  transports: ["websocket"],
+  transports: ["polling", "websocket"],
   reconnection: true,
   reconnectionAttempts: Infinity,
   reconnectionDelay: 1000,
--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import io from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
-  transports: ["websocket"],
+  transports: ["polling", "websocket"],
   reconnection: true,
   reconnectionAttempts: Infinity,
   reconnectionDelay: 1000,
